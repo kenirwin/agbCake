fd2d382a6d724cbedc914d7b7fc8107af138bf3f
Ken Irwin <kirwin@wittenberg.edu>
2017-06-05 06:00:11 -0700

    migration: changed image columns to better conform to upload example

---
+++ config/Migrations/20170605125058_change_portrait_column_names.php
@@ -0,0 +1,35 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class ChangePortraitColumnNames extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+      $table = $this->table('portraits');
+      $table->renameColumn('image_path','image_dir')
+	->renameColumn('image_filename','image')
+	->save();
+    }
+}

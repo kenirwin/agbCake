6462b5b55d9cf24f375d4e198155c99839bb7eb7
Ken Irwin <kirwin@wittenberg.edu>
2017-06-13 14:45:25 -0700

    allow nulls in roles table

---
+++ config/Migrations/20170613212918_AllowNullsInRoles.php
@@ -0,0 +1,25 @@
+<?php
+use Migrations\AbstractMigration;
+
+class AllowNullsInRoles extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+      $table = $this->table('roles');
+      $table->removeColumn('role');
+      $table->addColumn('role', 'string', ['null'=>'true','default' => 'null', 'after'=>'convent_id']);
+      $table->removeColumn('start_year');
+      $table->addColumn('start_year', 'string', ['null'=>'true','default' => 'null', 'after'=>'role']);
+      $table->removeColumn('end_year');
+      $table->addColumn('end_year', 'string', ['null'=>'true','default' => 'null', 'after'=>'start_year']);
+      $table->save();
+
+    }
+}

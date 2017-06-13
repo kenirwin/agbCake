14f1e3443e82cb87b76bd02d91901e27eeef3282
Ken Irwin <kirwin@wittenberg.edu>
2017-06-13 14:50:35 -0700

    baked all roles

---
+++ src/Model/Entity/Role.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Role Entity
+ *
+ * @property int $id
+ * @property int $woman_id
+ * @property int $convent_id
+ * @property string $role
+ * @property string $start_year
+ * @property string $end_year
+ * @property \Cake\I18n\FrozenTime $created
+ * @property \Cake\I18n\FrozenTime $modified
+ *
+ * @property \App\Model\Entity\Woman $woman
+ * @property \App\Model\Entity\Convent $convent
+ */
+class Role extends Entity
+{
+
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        '*' => true,
+        'id' => false
+    ];
+}
---
+++ tests/Fixture/RolesFixture.php
@@ -0,0 +1,61 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * RolesFixture
+ *
+ */
+class RolesFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'woman_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'convent_id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'role' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'start_year' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'end_year' => ['type' => 'string', 'length' => 255, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        '_indexes' => [
+            'woman_roles_fk' => ['type' => 'index', 'columns' => ['woman_id'], 'length' => []],
+            'convent_roles_fk' => ['type' => 'index', 'columns' => ['convent_id'], 'length' => []],
+        ],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+            'convent_roles_fk' => ['type' => 'foreign', 'columns' => ['convent_id'], 'references' => ['convents', 'id'], 'update' => 'restrict', 'delete' => 'restrict', 'length' => []],
+            'woman_roles_fk' => ['type' => 'foreign', 'columns' => ['woman_id'], 'references' => ['women', 'id'], 'update' => 'restrict', 'delete' => 'restrict', 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'woman_id' => 1,
+            'convent_id' => 1,
+            'role' => 'Lorem ipsum dolor sit amet',
+            'start_year' => 'Lorem ipsum dolor sit amet',
+            'end_year' => 'Lorem ipsum dolor sit amet',
+            'created' => '2017-06-13 21:50:10',
+            'modified' => '2017-06-13 21:50:10'
+        ],
+    ];
+}

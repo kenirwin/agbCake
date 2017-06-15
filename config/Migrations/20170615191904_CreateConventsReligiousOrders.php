f60b087136514c2de98c450c30c00a6138eda183
Ken Irwin <kirwin@wittenberg.edu>
2017-06-15 12:25:54 -0700

    added foreign keys to convents_religious_orders table

---
+++ config/Migrations/20170615191904_CreateConventsReligiousOrders.php
@@ -0,0 +1,37 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateConventsReligiousOrders extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function up()
+    {
+        $table = $this->table('convents_religious_orders');
+        $table->addColumn('convent_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->addColumn('relgious_order_id', 'integer', [
+            'default' => null,
+            'limit' => 11,
+            'null' => false,
+        ]);
+        $table->create();
+	
+	$table->addForeignKey('convent_id','convent' array('id'),array('constraint'=>'convent_order_fk'));
+	$table->addForeignKey('religious_order_id','religious_orders', array('id'),array('constraint'=>'religious_order_convent_fk'));
+	$table->save();
+    }
+
+    public function down() 
+    {
+      $table= $this->dropTable('convents_religious_orders');
+    }
+}
